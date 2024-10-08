27f06282aa4c128085d93d7e499081351e148f9b
DEVictorMaia <maiavictorh.dev@gmail.com>
2024-10-08 21:50:45 +0100

    Are you still reading?

--- C_Projects/C03/ft_strcat.c
+++ C_Projects/C03/ft_strcat.c
@@ -1,3 +1,28 @@
 #include <stdio.h>
 
-char    *ft_strcat(char *dest, char *src) {}
+char    *ft_strcat(char *dest, char *src)
+{
+    char *end;
+
+    end = dest;
+    while (*end != '\0')
+        end++;
+    while (*src != '\0')
+    {
+        *end = *src;
+        end++;
+        src++;
+    }
+    *end = '\0';
+    return (dest);
+}
+
+int main()
+{
+    char s1[] = "Victor ";
+    char s2[] = "Maia";
+
+    printf (" Sorce string: %s;\n", s1);
+    printf (" Destination string before the concatenation: %s;\n", s2);
+    printf (" Concatenation result: %s;\n", ft_strcat(s1, s2));
+}
--- C_Projects/C03/ft_strncat.c
+++ C_Projects/C03/ft_strncat.c
@@ -1,3 +1,27 @@
 #include <stdio.h>
 
-char    *ft_strncat(char *dest, char *src, unsigned int nb) {}
+char    *ft_strncat(char *dest, char *src, unsigned int nb)
+{
+    unsigned int i = 0;
+    unsigned int c = 0;
+
+    while (dest[i] != '\0')
+        i++;
+    while (src[c] != '\0' && c < nb)
+    {
+        dest[i] = src[c];
+        i++;
+        c++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
+
+int main()
+{
+    char s1[] = "Victor ";
+    char s2[] = "Hugo Maia";
+
+    printf(" Strings before the concatenation:\n   %s;\n   %s;\n", s1, s2);
+    printf(" Concatenating the first 4 letters: %s\n", ft_strncat(s1, s2, 4));
+}
